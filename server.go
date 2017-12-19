e8da9ae3fb2e4a75eec5e5c790c71720339e8618
eriklindqvist <erikjo82+github@gmail.com>
2017-12-19 10:19:33 +0100

    Minimal HTTP server in go

---
+++ server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	l "github.com/eriklindqvist/recepies/app/lib"
+)
+
+func main() {
+	log.Printf("Server started")
+
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(l.Getenv("FILEBASE", "files")))))
+
+	if err := http.ListenAndServe(":" + l.Getenv("PORT", "3003"), nil); err != nil {
+    log.Fatal("ListenAndServe: ", err)
+  }
+}
